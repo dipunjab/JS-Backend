0fe59336d5a95c242b81d33c90139923a98867db
Usman <ghaniusman0287@gmail.com>
2024-06-20 16:17:17 +0500

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+
+import mongoose from "mongoose"
+import {DB_NAME} from "./constants.js"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+//first approch
+/*
+import express from "express"
+const app =express()
+
+( async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.log("Error: ", error);
+       })
+
+       app.listen(process.env.PORT,()=>{
+        console.log('App is listening on port'+process.env.PORT);
+       })
+
+    } catch (err) {
+        console.error("Error: "+err);
+        throw err
+    }
+} 
+)();
+*/
