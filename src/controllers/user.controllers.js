828f5f012414ed7053baec50081abcde13c66d7a
Usman <ghaniusman0287@gmail.com>
2024-06-25 09:51:43 +0500

    minor mistakes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -255,7 +255,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res.status(200)
-    .json(200,req.user,"Current user fetched successfully")
+    .json(new ApiResponse(200,req.user,"Current user fetched successfully"))
 });
 
 const updateAccountDetail = asyncHandler(async (req, res) => {
@@ -265,7 +265,7 @@ const updateAccountDetail = asyncHandler(async (req, res) => {
         throw new ApiError(400,"All fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
